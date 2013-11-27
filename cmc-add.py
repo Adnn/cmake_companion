5dab8dae2063062ba6a4b4d6fb71251099aa92af
Adrien <adrien@astutegraphics.co.uk>
2013-11-27 16:02:26 +0100

    Adds the '--headers' option, to create header files only.

--- cmc-add.py
+++ cmc-add.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description = 'Add classes to a project.')
     parser.add_argument ("classname", nargs="+", help="The name of the class that will be added to the current project")
+    parser.add_argument ("--headers", action="store_true", help="Create header files only for all lister class names.")
 
     args = parser.parse_args()
 
@@ -18,10 +19,16 @@ if __name__ == '__main__':
 
     output_file = ""
 
+    if args.headers :
+        filtered_data = [metadata[0]]
+    else :
+        filtered_data = metadata
+
     with open("CMakeLists.txt", "r") as cmfile:
             text_file = cmfile.read()
             
-            for entry in metadata:
+
+            for entry in filtered_data :
                 match = re.search(entry[0], text_file, re.DOTALL)
                 found_dict[match.end()] = entry + (match,)
 
