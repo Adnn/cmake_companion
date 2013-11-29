4742e311f7b1e6defe6a8d369e89facf51b5c4cc
Adrien <adrien@astutegraphics.co.uk>
2013-11-29 17:20:15 +0100

    Major refactoring into classes, adds the ability to delete a name.

--- cmc-add.py
+++ cmc-add.py
@@ -1,29 +1,115 @@
 #!/usr/bin/python
 
-import re, argparse
+import re, argparse, os
 
-metadata = [
-("(?P<declaration>set(\w)*\((\w)*\${PROJECT_NAME}_HEADERS)(?P<files>.*?)\)", ".h"),
-("(?P<declaration>set(\w)*\((\w)*\${PROJECT_NAME}_SOURCES)(?P<files>.*?)\)", ".cpp"),
-]
+class CMakeFile:
+    metadata = {
+        "headers" :         {"regex" : "(?P<declaration>set(\w)*\((\w)*\${PROJECT_NAME}_HEADERS)(?P<files>.*?)\)", "ext" : ".h"},
+        "implementations" : {"regex" : "(?P<declaration>set(\w)*\((\w)*\${PROJECT_NAME}_SOURCES)(?P<files>.*?)\)", "ext" : ".cpp"},
+        }
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.outro = "";
+        self.groups = [];
+
+        with open(self.filename, "r") as cmfile:
+            text_file = cmfile.read()
+
+            found_dict = {}
+            for key, value in CMakeFile.metadata.iteritems():
+                match = re.search(value["regex"], text_file, re.DOTALL)
+                found_dict[match.end()] = value
+                found_dict[match.end()].update({"match" : match})
+                found_dict[match.end()].update({"category" : key})
+
+            previous_match_end = 0
+
+            for key in sorted(found_dict.keys()):
+                entry = found_dict[key]
+                match = entry["match"] 
+                self.groups.append( (text_file[previous_match_end:match.start()],
+                                     match.group("declaration"),
+                                     FileList(match.group("files"), entry["category"]),
+                                     "\n)", 
+                                     ))
+                previous_match_end = match.end()
+
+            self.outro = text_file[previous_match_end:]
+
+    def insert_names(self, name_list, category_filter_list):
+        new_files = \
+            [file_list.insert_names(name_list)
+            for file_list in [group[2] for group in self.groups]
+            if file_list.category in category_filter_list]
+
+        [open(filename, "a") for filename_list in new_files for filename in filename_list]
+
+    def remove_names(self, name_list):
+        removed_files = \
+            [group[2].remove_names(name_list) for group in self.groups]
+        
+        [os.remove(filename) for filename_list in removed_files for filename in filename_list]
+
+    def save_file(self):
+        output_file = ''.join(str(i) for group in self.groups for i in group)
+        output_file = output_file + self.outro
+        print output_file
+
+        with open(self.filename, "w") as cmfile:
+            cmfile.write(output_file)
+
+
+class FileList:
+    def __init__(self, string_list, category):
+        self.category = category
+        self.metaref = CMakeFile.metadata[self.category]
+        self.file_list = string_list.split()
+
+    def insert_names(self, name_list):
+        new_names = [basename + self.metaref["ext"]
+                     for basename in name_list]
+        self.file_list.extend(new_names)
+        self.file_list = sorted(self.file_list)
+        return new_names
+
+    def remove_names(self, name_list) :
+        removed_names = [name 
+                         for name in [basename + self.metaref["ext"] for basename in name_list]
+                         if name in self.file_list]
+        [self.file_list.remove(name) for name in removed_names]
+        return removed_names
+        
+    def __str__(self):
+        #prepend an empty element, to get the separator before the first elem in file_list
+        return "\n    ".join(['']+self.file_list)
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description = 'Add classes to a project.')
     parser.add_argument ("classname", nargs="+", help="The name of the class that will be added to the current project")
-    parser.add_argument ("--headers", action="store_true", help="Create header files only for all lister class names.")
+    parser.add_argument ("--headers", action="store_true", help="Create header files only for listed names.")
+    parser.add_argument ("--remove", action="store_true", help="Remove the entries from the CMake file, and delete the corresponding files from the directory.")
 
     args = parser.parse_args()
 
-    found_dict = {}
+    cm_file = CMakeFile("CMakeLists.txt")
 
-    output_file = ""
-
-    if args.headers :
-        filtered_data = [metadata[0]]
+    if args.remove :
+        cm_file.remove_names(args.classname)
     else :
-        filtered_data = metadata
+        if args.headers :
+            filter = ["headers"]
+        else :
+            filter = ["headers", "implementations"]
 
+        cm_file.insert_names(args.classname, filter)
+
+    cm_file.save_file()
+
+    
+'''
     with open("CMakeLists.txt", "r") as cmfile:
             text_file = cmfile.read()
             
@@ -38,7 +124,7 @@ if __name__ == '__main__':
             for key in sorted(found_dict.keys()):
                 match = found_dict[key][2]
 
-                updated_list = match.group("files").split()
+                updated_list = match.group("files")
                 new_filenames = [basename + found_dict[key][1] for basename in args.classname]
                 [open(filename, "a") for filename in new_filenames]
                 updated_list.extend(new_filenames)
@@ -52,4 +138,4 @@ if __name__ == '__main__':
     
     with open("CMakeLists.txt", "w") as cmfile:
         cmfile.write(output_file)
-
+'''
